e54d5a964612a9dae76dca94c76f985c7d4362ca
Massaki Archambault <badjware@massaki.ca>
2018-09-20 20:41:59 -0400

    replaced polybar with i3bar

--- files/.config/i3/startup.bash
+++ files/.config/i3/startup.bash
@@ -4,6 +4,6 @@
 $HOME/.fehbg || feh --bg-fill --no-xinerama /usr/share/pixmaps/wallpaper.jpg
 
 # Initial workspace and startup app
-i3-msg "workspace 2"
+i3-msg workspace number "2: 2 "
 i3-sensible-terminal
 

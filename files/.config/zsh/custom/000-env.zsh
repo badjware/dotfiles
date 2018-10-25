16f294cbf21dd7c117193508e2695a0714f097ec
Massaki Archambault <badjware@massaki.ca>
2018-10-25 18:27:08 -0400

    reroll hostname prompt color if too dark

--- files/.config/zsh/custom/000-env.zsh
+++ files/.config/zsh/custom/000-env.zsh
@@ -23,5 +23,23 @@ export IS_ROOT
 export GPG_TTY=$(tty)
 gpg-connect-agent updatestartuptty /bye >/dev/null
 
-# Color
-export ZSH_THEME_HOSTNAME_COLOR="$(printf "%03d" "$(hostname | md5sum | head -c2)")"
+# Color for hostname
+function get_hostname_color () {
+    local col
+    for hash_func in md5sum sha1sum sha256sum; do
+        col="$(printf "%03d" "$((16#$(hostname | $hash_func | head -c2)))")"
+        if [[ $col -ne 0 ]] \
+        && [[ $col -lt 16 || $col -gt 21 ]] \
+        && [[ $col -lt 232 || $col -gt 243 ]]; then
+            # we found a color that isn't too dark
+            echo $col>>~/test.log
+            break
+        else
+            # default color
+            col="002"
+        fi
+    done
+    printf "$col"
+}
+export ZSH_THEME_HOSTNAME_COLOR="$(get_hostname_color)"
+unset -f get_hostname_color

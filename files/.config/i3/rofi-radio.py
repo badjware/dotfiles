7fbce2960bd277a740fe19ee5dabbeeb2ae27d4b
Massaki Archambault <badjware@massaki.ca>
2018-11-08 10:49:50 -0500

    playlist file support

--- files/.config/i3/rofi-radio.py
+++ files/.config/i3/rofi-radio.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 import json
-import yaml
 import os
-import sys
-
 from subprocess import Popen, PIPE, check_output, check_call
+
+import yaml
 
 def mpv_ipc(*args):
     try:
@@ -15,7 +14,7 @@ def mpv_ipc(*args):
         pass
     return ''
 
-with open(os.path.expanduser("~/.local/share/playlists.yaml")) as y:
+with open(os.path.expanduser("~/.local/share/rofi-radio/playlists.yaml")) as y:
     stations = yaml.load(y)
 
 rofi_command = ['rofi', '-i', '-selected-row', '0', '-dmenu', '-p', 'mpv']
@@ -23,7 +22,7 @@ rofi_command = ['rofi', '-i', '-selected-row', '0', '-dmenu', '-p', 'mpv']
 current_song = mpv_ipc('get_property_string', 'media-title')
 if current_song:
     rofi_command += ['-mesg', current_song]
-    commands = b"Play/Pause\nStop\nNext\nPrevious"
+    commands = b"Play/Pause\nStop\nShuffle\nNext\nPrevious"
 else:
     commands = bytes('\n'.join([s["name"] for s in stations]), 'utf8')
 
@@ -35,12 +34,29 @@ if current_song:
         mpv_ipc('cycle', 'pause')
     elif choice == "Stop":
         mpv_ipc('stop')
+    elif choice == "Shuffle":
+        mpv_ipc('playlist-shuffle')
+    elif choice == "Next":
+        mpv_ipc('playlist-next')
+    elif choice == "Previous":
+        mpv_ipc('playlist-prev')
 else:
     s = [s for s in stations if s["name"] == choice]
     if s:
-        mpv_ipc('loadfile', s[0]["loc"])
-        mpv_ipc('set_property_string', 'pause', 'no')
+        # Load the selected playlist
+        loc = s[0]["loc"]
+        if loc.startswith('http') or loc.startswith('ytdl'):
+            # Load the url
+            mpv_ipc('loadfile', loc)
+        else:
+            # Load the file
+            workdir = mpv_ipc('get_property_string', 'working-directory')
+            mpv_ipc('loadfile', os.path.expanduser(loc))
+    else:
+        # Try to load the user input
+        mpv_ipc('loadfile', choice)
+    mpv_ipc('set_property_string', 'pause', 'no')
+
 
 # force update of i3status
-check_call('killall', '-USR1', 'i3status')
-
+check_call(['killall', '-USR1', 'i3status'])

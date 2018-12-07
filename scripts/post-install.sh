80a5b8b523ee0bdb39cb40e4e08429a56e406415
Massaki Archambault <badjware@massaki.ca>
2018-12-06 19:30:53 -0500

    fix post-install ordering

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# install vim plugins
-vim -E +"call dein#update()" +"qall!" /dev/null &>/dev/null 
+# run units
+systemctl --user daemon-reload
+systemctl --user enable --now tmux.service
+systemctl --user enable --now mpv.service
 
 # install tmux plugins
 tmux run-shell ~/.tmux/plugins/tpm/bin/install_plugins
 tmux run-shell ~/.tmux/plugins/tpm/bin/clean_plugins
 
-systemctl --user daemon-reload
-systemctl --user enable --now tmux.service
-systemctl --user enable --now mpv.service
+# install vim plugins
+vim -E +"call dein#update()" +"qall!" /dev/null &>/dev/null 
 

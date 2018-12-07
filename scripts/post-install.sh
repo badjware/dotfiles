597984968f279b6c9bb65b264d0d979baf93cfa5
Massaki Archambault <badjware@massaki.ca>
2018-12-06 19:25:18 -0500

    fix tmux-continuum, autostart tmux

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -3,5 +3,11 @@
 # install vim plugins
 vim -E +"call dein#update()" +"qall!" /dev/null &>/dev/null 
 
-systemctl --user enable mpv.service
+# install tmux plugins
+~/.tmux/plugins/tpm/bin/install_plugins
+~/.tmux/plugins/tpm/bin/clean_plugins
+
+systemctl --user daemon-reload
+systemctl --user enable --now tmux.service
+systemctl --user enable --now mpv.service
 

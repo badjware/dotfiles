d13cbc7a6171d5f09e3b7705b859660be3e17075
Massaki Archambault <badjware@massaki.ca>
2019-09-28 20:06:57 -0400

    switch to mako from dunst

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -5,6 +5,7 @@ systemctl --user daemon-reload
 systemctl --user enable --now redshift.service
 systemctl --user enable --now swayidle.service
 systemctl --user enable --now wob.service
+systemctl --user enable --now mako.service
 
 # install tmux plugins
 tmux run-shell ~/.tmux/plugins/tpm/bin/install_plugins

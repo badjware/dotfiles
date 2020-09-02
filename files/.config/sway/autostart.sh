70ddf4e58db246f2a2e9233d0ca0952e38c80ea2
Massaki Archambault <badjware@massaki.ca>
2020-09-02 15:47:17 -0400

    update sway config, enable freesync

---
+++ files/.config/sway/autostart.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# Autostart sway if we are on tty1
+if [ -z "$WAYLAND_DISPLAY" ] && [ "$(tty)" = "/dev/tty1" ]; then
+    # setup logging
+    log_stdout="$XDG_RUNTIME_DIR/sway_stdout.log"
+    log_stderr="$XDG_RUNTIME_DIR/sway_stderr.log"
+    echo "Redirecting stdout to $log_stdout"
+    echo "Redirecting stderr to $log_stderr"
+
+    exec ~/bin/sway 2>"$log_stderr" >"$log_stdout"
+fi
+

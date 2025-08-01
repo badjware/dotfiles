4075b0a5f1b4b3ec40dbad6fab818f7c227c0858
Massaki Archambault <marchambault@badjware.dev>
2025-07-31 23:15:59 -0400

    update package list

--- home/run_once_setup-ohmyzsh.sh
+++ home/run_once_setup-ohmyzsh.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export ZSH="$HOME/.config/zsh/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone  https://github.com/zdharma-continuum/fast-syntax-highlighting $HOME/.config/zsh/custom/plugins/fast-syntax-highlighting

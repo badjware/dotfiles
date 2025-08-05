bfa18d1d3108b9fae3021a68ca9c7d3e09a25e6a
Massaki Archambault <marchambault@badjware.dev>
2025-08-05 15:36:10 -0400

    Add .chezmoiexternals/vim.toml Add .chezmoiexternals/wallpaper.toml Add .chezmoiexternals/zsh.toml Add .config/environment.d/app.conf Update .config/environment.d/wayland.conf Update .profile Update .zshrc Remove setup-ohmyzsh.sh Remove setup-vim.sh Remove wallpaper.sh

--- home/run_once_setup-vim.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INSTALL_DIR="$HOME/.vim/bundle/repos/github.com/Shougo/dein.vim"
-
-if [ ! -d "$INSTALL_DIR" ]; then
-    mkdir -p "$(dirname $INSTALL_DIR)" || true
-    git clone https://github.com/Shougo/dein.vim "$INSTALL_DIR"
-fi
--- home/run_wallpaper.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [[ ! -e "$HOME/.local/share/wallpaper" ]]; then
-    curl -o $HOME/.local/share/wallpaper https://badjware.dev/public/wallpaper
-fi

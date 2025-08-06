80172001950aab9bcdb5e9d0f75521563a1a0dfa
Massaki Archambault <marchambault@badjware.dev>
2025-08-06 10:10:43 -0400

    set pay-respect to use local ai

--- home/dot_config/zsh/custom/302-pay-respects.zsh
+++
@@ -1,6 +0,0 @@
-eval "$(pay-respects zsh --alias)"
-
-export _PR_AI_DISABLE=true
-export _PR_AI_URL="https://chat.badjware.dev/api/chat/completions"
-export _PR_AI_API_KEY=""
-export _PR_AI_MODEL="Gemma-3n-4B"
---
+++ home/dot_config/zsh/custom/create_302-pay-respects.zsh
@@ -0,0 +1,7 @@
+eval "$(pay-respects zsh --alias)"
+
+export _PR_AI_DISABLE=true # unset to enable
+# export _PR_AI_URL="http://localhost:11434/api/chat" # local ollama
+export _PR_AI_URL="https://chat.badjware.dev/api/chat/completions"
+export _PR_AI_API_KEY=""
+export _PR_AI_MODEL="Qwen3-Coder-30B-A3B"

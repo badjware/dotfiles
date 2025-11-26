a3b23a116f11b319b32a159c449efb9782994f88
Massaki Archambault <marchambault@badjware.dev>
2025-11-25 22:28:34 -0500

    tweak gamemode script

--- home/.chezmoiscripts/run_steamvr.sh
+++ home/.chezmoiscripts/run_steamvr.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-sudo setcap CAP_SYS_NICE+ep ./.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrcompositor-launcher
+VRCOMPOSITOR_LAUNCHER="./.local/share/Steam/steamapps/common/SteamVR/bin/linux64/vrcompositor-launcher"
+
+
+if [[ -f "$VRCOMPOSITOR_LAUNCHER" ]]; then
+    sudo setcap CAP_SYS_NICE+ep "$VRCOMPOSITOR_LAUNCHER"
+fi

3fee46708b1ea6d4f2a3930208a7584769581707
Massaki Archambault <marchambault@badjware.dev>
2026-03-05 11:43:58 -0500

    style waybar urgent workspace

--- home/.chezmoiscripts/run_firefox_policies.sh
+++ home/.chezmoiscripts/run_firefox_policies.sh
@@ -7,7 +7,7 @@ sudo tee /etc/firefox/policies/policies.json >/dev/null <<EOD
         "GenerativeAI": {
             "LinkPreviews": false
         },
-        "PasswordManagerEnabled": false
+        "PasswordManagerEnabled": false,
         "Preferences": {
             "browser.ml.chat.provider": "https://chat.badjware.dev/?temporary-chat=true&model=Qwen3.5-35B-A3B"
         }

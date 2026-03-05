1d51771d4be581837355fd39e72f1abefa39b186
Massaki Archambault <marchambault@badjware.dev>
2026-03-05 15:00:35 -0500

    custom prompt

--- home/.chezmoiscripts/run_firefox_policies.sh
+++ home/.chezmoiscripts/run_firefox_policies.sh
@@ -4,13 +4,12 @@ sudo mkdir -p /etc/firefox
 sudo tee /etc/firefox/policies/policies.json >/dev/null <<EOD
 {
     "policies": {
-        "GenerativeAI": {
-            "LinkPreviews": false
-        },
         "PasswordManagerEnabled": false,
         "Preferences": {
             "browser.tabs.insertRelatedAfterCurrent": false,
-            "browser.ml.chat.provider": "https://chat.badjware.dev/?temporary-chat=true&model=Qwen3.5-35B-A3B"
+            "browser.ml.chat.provider": "https://chat.badjware.dev/?temporary-chat=true&model=browser-assistant",
+            "browser.ml.chat.prompts.3": "{\"label\":\"Validate code snippet\", \"value\":\"Please validate the selected code snippet. Assume any HTML entities are represented as their corresponding characters in the actual code. Assume that this snippet is part of a larger codebase. Identify and report any mistakes, syntax errors, and potential improvements. Do not report correct or reasonable code. Please ask if you require more context.\",\"targeting\":\"contentType != 'page'\"}",
+            "browser.ml.linkPreview.enabled": false
         }
     }
 }

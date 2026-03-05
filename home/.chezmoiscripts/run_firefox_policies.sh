a9f4bd3e145bf6779e92d79347a18a42860e9eb1
Massaki Archambault <marchambault@badjware.dev>
2026-03-05 14:15:48 -0500

    set browser.tabs.insertRelatedAfterCurrent to false

--- home/.chezmoiscripts/run_firefox_policies.sh
+++ home/.chezmoiscripts/run_firefox_policies.sh
@@ -9,6 +9,7 @@ sudo tee /etc/firefox/policies/policies.json >/dev/null <<EOD
         },
         "PasswordManagerEnabled": false,
         "Preferences": {
+            "browser.tabs.insertRelatedAfterCurrent": false,
             "browser.ml.chat.provider": "https://chat.badjware.dev/?temporary-chat=true&model=Qwen3.5-35B-A3B"
         }
     }

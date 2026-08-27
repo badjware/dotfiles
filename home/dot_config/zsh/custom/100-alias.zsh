30fffa327845ce5fcce3303bacfbaaa62437db57
Massaki Archambault <marchambault@badjware.dev>
2026-08-27 12:31:54 -0400

    update build-and-run

--- home/dot_config/zsh/custom/100-alias.zsh
+++ home/dot_config/zsh/custom/100-alias.zsh
@@ -12,9 +12,9 @@ alias userctl="systemctl --user"
 alias etckeeper="sudo etckeeper"
 
 fpath=(~/develop/github.com/badjware/pibox/completions $fpath)
-pibox() { ~/develop/github.com/badjware/pibox/launch.sh --sac-moe-patience --unsafe-enable-docker --unsafe-host-wayland --harness pi -v "$HOME/go/pkg:$HOME/go/pkg" "$@" }
-claudebox() { ~/develop/github.com/badjware/pibox/launch.sh --sac-moe-patience --unsafe-enable-docker --unsafe-host-wayland --harness claude -v "$HOME/go/pkg:$HOME/go/pkg" "$@" }
-hermesbox() { ~/develop/github.com/badjware/pibox/launch.sh --sac-moe-patience --unsafe-enable-docker --unsafe-host-wayland --harness hermes -v "$HOME/go/pkg:$HOME/go/pkg" "$@" }
+pibox() { ~/develop/github.com/badjware/pibox/launch.sh --sac-moe-patience --unsafe-enable-docker --unsafe-host-wayland --harness pi -v "$HOME/go:$HOME/go" "$@" }
+claudebox() { ~/develop/github.com/badjware/pibox/launch.sh --sac-moe-patience --unsafe-enable-docker --unsafe-host-wayland --harness claude -v "$HOME/go:$HOME/go" "$@" }
+nanobox() { ~/develop/github.com/badjware/pibox/launch.sh --sac-moe-patience --unsafe-host-net --unsafe-host-wayland --harness nanobot -v "$HOME/go:$HOME/go" "$@" }
 
 # ls
 alias ls='lsd --group-dirs=first'
--- home/dot_pi/agent/prompts/build-and-test.md
+++ home/dot_pi/agent/prompts/build-and-test.md
@@ -1,4 +1,4 @@
 ---
 description: Build and run tests in current project
 ---
-Build the current project and run its test suite. Report any build errors or test failures clearly. Do not apply any fixes.
+Build the current project, run the linter, and run its test suite. If available, read the ci pipeline configuration for guidance. Report the result of the build, lint, and tests clearly and concisely in a table. Do not apply any fixes.
